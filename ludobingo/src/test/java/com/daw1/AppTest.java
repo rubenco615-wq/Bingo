b3c64e9a630ad87e208a730931d4d14b6c039194
rubenco615-wq <rubenco615@alumnos.ilerna.com>
2026-03-12 12:42:46 +0100

    Some packages fixed

--- ludobingo/src/test/java/com/daw1/AppTest.java
+++ ludobingo/src/test/java/com/daw1/AppTest.java
@@ -1,3 +1,4 @@
+//TODO: DELETE THIS ONCE WE HAVE PROPER TESTS
 package com.daw1;
 
 import static org.junit.Assert.assertTrue;
@@ -7,14 +8,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
